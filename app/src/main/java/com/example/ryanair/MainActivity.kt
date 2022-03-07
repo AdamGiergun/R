e7d82fb12d0026b3abdfaef8d7c846970fcddefb
Adam Giergun <adam.giergun@onet.pl>
2022-03-07 23:07:50 +0100

    Implement DataBinding and assign ViewModel

--- app/src/main/java/com/example/ryanair/MainActivity.kt
+++ app/src/main/java/com/example/ryanair/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.ryanair
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.ryanair.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ActivityMainBinding.inflate(layoutInflater).run {
+            setContentView(root)
+            lifecycleOwner = this@MainActivity
+            viewModel = mainViewModel
+        }
     }
 }
