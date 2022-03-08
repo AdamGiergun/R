5cfd234c9ba6c3d2956478fdcfd0848e0c338776
Adam Giergun <adam.giergun@onet.pl>
2022-03-08 22:38:40 +0100

    Add route LiveData

--- app/src/main/java/com/example/ryanair/MainViewModel.kt
+++ app/src/main/java/com/example/ryanair/MainViewModel.kt
@@ -4,17 +4,24 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.ryanair.db.Route
 import com.example.ryanair.db.Station
+import com.example.ryanair.repository.RouteRepository
 import com.example.ryanair.repository.StationsRepository
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     private val stationsRepository = StationsRepository()
+    private val routeRepository = RouteRepository()
 
     private val _stations = MutableLiveData<List<Station>>()
     val stations: LiveData<List<Station>>
         get() = _stations
+
+    private val _route = MutableLiveData<Route>()
+    val route: LiveData<Route>
+        get() = _route
 
     private val _text = MutableLiveData<String>()
     val text: LiveData<String>
@@ -22,15 +29,29 @@ class MainViewModel : ViewModel() {
 
     init {
         viewModelScope.launch {
-            var newText = ""
-            stationsRepository.refresh()?.let { newText = it }
-            _text.value = stationsRepository.run {
-                _stations.value = stations
-                stations.forEach { station ->
-                    newText += "${station.code}\n"
-                }
-                newText
+            val text1 = initStations()
+            val text2 = initRoute()
+            _text.value = "$text1\n$text2"
+        }
+    }
+
+    private suspend fun initStations(): String {
+        var newText = ""
+        stationsRepository.refresh()?.let { newText = it }
+        stationsRepository.run {
+            _stations.value = stations
+            stations.forEach { station ->
+                newText += "${station.code} "
             }
         }
+        return newText
+    }
+
+
+    private suspend fun initRoute(): String {
+        var newText = ""
+        routeRepository.refresh()?.let { newText = it }
+        newText += routeRepository.route.toString()
+        return newText
     }
 }
