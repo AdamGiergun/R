ad78f4e869dedc63a2518c65e31799e2035362f4
Adam Giergun <adam.giergun@onet.pl>
2022-03-08 20:44:21 +0100

    Add Route tests

--- app/src/test/java/com/example/ryanair/network/DataTransferObjectsTest.kt
+++ app/src/test/java/com/example/ryanair/network/DataTransferObjectsTest.kt
@@ -1,27 +1,16 @@
 package com.example.ryanair.network
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.example.ryanair.MainCoroutineRule
 import com.example.ryanair.MockResponseFileReader
 import com.squareup.moshi.Moshi
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
 
-@ExperimentalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class)
 class DataTransferObjectsTest {
-
-    @get:Rule
-    var instantExecutorRule = InstantTaskExecutorRule()
-
-    @get:Rule
-    var mainCoroutineRule = MainCoroutineRule()
 
     @Test
     fun `read sample stations json file`() {
@@ -30,12 +19,40 @@ class DataTransferObjectsTest {
     }
 
     @Test
-    fun `convert sample stations json file to stations list`() = runTest {
+    fun `convert sample stations json file to StationJson list`() {
+        val reader = MockResponseFileReader("stations.json")
+        val moshi: Moshi = Moshi.Builder().build()
+        val adapter = moshi.adapter(StationsContainer::class.java)
+        val stations = adapter.fromJson(reader.content)?.stations
+        assertThat(stations, `is`(notNullValue()))
+        assertThat(stations?.size ?: 0, `is`(1))
+        assertThat(stations?.first()?.code ?: "", `is`("AAL"))
+    }
+
+    @Test
+    fun `convert sample stations json file to Station list`() {
         val reader = MockResponseFileReader("stations.json")
         val moshi: Moshi = Moshi.Builder().build()
         val adapter = moshi.adapter(StationsContainer::class.java)
         val stations = adapter.fromJson(reader.content)?.asDbModel() ?: emptyList()
         assertThat(stations, `is`(notNullValue()))
         assertThat(stations.size, `is`(1))
+        assertThat(stations.first().code, `is`("AAL"))
+    }
+
+    @Test
+    fun `read sample route json file`() {
+        val reader = MockResponseFileReader("route.json")
+        assertThat(reader.content, `is`(notNullValue()))
+    }
+
+    @Test
+    fun `convert sample route json file to RouteJson`() {
+        val reader = MockResponseFileReader("route.json")
+        val moshi: Moshi = Moshi.Builder().build()
+        val adapter = moshi.adapter(RouteJson::class.java)
+        val route = adapter.fromJson(reader.content)
+        assertThat(route, `is`(notNullValue()))
+        assertThat(route?.termsOfUse, `is`("https://www.ryanair.com/ie/en/corporate/terms-of-use=AGREED"))
     }
 }
