a8a4d64f23492563873eec3d629e6942791cd493
Adam Giergun <adam.giergun@onet.pl>
2022-03-08 22:36:05 +0100

    Add method for getting example Route with REST Api

--- app/src/main/java/com/example/ryanair/network/RyanairService.kt
+++ app/src/main/java/com/example/ryanair/network/RyanairService.kt
@@ -5,9 +5,10 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val STATIONS_URL = " https://mobile-testassets-dev.s3.eu-west-1.amazonaws.com/"
-//private const val FLIGHTS_BASE_URL = "https://www.ryanair.com/api/booking/v4/en-gb/"
+private const val FLIGHTS_BASE_URL = "https://www.ryanair.com/api/booking/v4/en-gb/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -18,36 +19,36 @@ private val retrofitStations = Retrofit.Builder()
     .baseUrl(STATIONS_URL)
     .build()
 
-//private val retrofitRoute = Retrofit.Builder()
-//    .addConverterFactory(MoshiConverterFactory.create(moshi))
-//    .baseUrl(FLIGHTS_BASE_URL)
-//    .build()
+private val retrofitRoute = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(FLIGHTS_BASE_URL)
+    .build()
 
 interface StationsApiService {
     @GET("stations.json")
     suspend fun getAirports(): StationsContainer
 }
 
-//interface RouteApiService {
-//    @GET("Availability")
-//    suspend fun getRoute(
-//        @Query("dateout") dateOut: String,
-//        @Query("origin") origin: String,
-//        @Query("destination") destination: String,
-//        @Query("adt") adult: Int,
-//        @Query("chd") child: Int,
-//        @Query("teen") teen: String,
-//        @Query("inf") inf: String,
-//        @Query("ToUs") ToUs: String
-//    ): RouteJson
-//}
+interface RouteApiService {
+    @GET("Availability")
+    suspend fun getRoute(
+        @Query("dateout") dateOut: String,
+        @Query("origin") origin: String,
+        @Query("destination") destination: String,
+        @Query("adt") adult: Int,
+        @Query("chd") child: Int,
+        @Query("teen") teen: Int,
+        @Query("inf") inf: Int,
+        @Query("ToUs") ToUs: String
+    ): RouteJson
+}
 
 object RyanairApi {
     val retrofitStationsApiService: StationsApiService by lazy {
         retrofitStations.create(StationsApiService::class.java)
     }
 
-//    val retrofitRouteApiService: RouteApiService by lazy {
-//        retrofitFlights.create(RouteApiService::class.java)
-//    }
+    val retrofitRouteApiService: RouteApiService by lazy {
+        retrofitRoute.create(RouteApiService::class.java)
+    }
 }
