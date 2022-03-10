9d255d4b19a598765288fad048837601ee85f183
Adam Giergun <adam.giergun@onet.pl>
2022-03-10 11:24:57 +0100

    Add first tests of MainViewModel

---
+++ app/src/test/java/com/example/ryanair/repository/MockStationsRepositoryImpl.kt
@@ -0,0 +1,34 @@
+package com.example.ryanair.repository
+
+import com.example.ryanair.MockResponseFileReader
+import com.example.ryanair.db.Station
+import com.example.ryanair.network.StationsContainer
+import com.example.ryanair.network.asDbModel
+import com.squareup.moshi.Moshi
+
+class MockStationsRepositoryImpl : StationsRepository {
+
+    override lateinit var stations: List<Station>
+        private set
+
+    override var error = false
+        private set
+
+    override var errorText: String? = null
+        private set
+
+    lateinit var reader: MockResponseFileReader
+
+    override suspend fun refresh() {
+        try {
+            val moshi: Moshi = Moshi.Builder().build()
+            val adapter = moshi.adapter(StationsContainer::class.java)
+            stations = adapter.fromJson(reader.content)?.asDbModel() ?: emptyList()
+            errorText = null
+            error = false
+        } catch (e: Exception) {
+            errorText = e.localizedMessage
+            error = true
+        }
+    }
+}
---
+++ app/src/test/java/com/example/ryanair/view/MainViewModelTest.kt
@@ -0,0 +1,59 @@
+package com.example.ryanair
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.ryanair.repository.MockStationsRepositoryImpl
+import com.example.ryanair.view.MainViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class MainViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private var viewModel: MainViewModel? = null
+
+    private val repo = MockStationsRepositoryImpl()
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+        viewModel = null
+    }
+
+    @Test
+    fun `provide view model with sample stations json file and check stations value`() = runTest {
+        repo.reader = MockResponseFileReader("stations.json")
+        MainViewModel(repo).run {
+            advanceUntilIdle()
+            MatcherAssert.assertThat(stations.value, `is`(notNullValue()))
+            stations.value?.let { viewModelStations ->
+                MatcherAssert.assertThat(viewModelStations.size, `is`(1))
+                MatcherAssert.assertThat(viewModelStations.first().code, `is`("AAL"))
+            }
+        }
+    }
+
+    @Test
+    fun `provide view model with fail json file and check stations value is null`() = runTest {
+        repo.reader = MockResponseFileReader("fail.json")
+        MainViewModel(repo).run {
+            advanceUntilIdle()
+            MatcherAssert.assertThat(stations.value, `is`(nullValue()))
+        }
+    }
+}
