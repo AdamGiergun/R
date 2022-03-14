0122822f4c0b00f28c1206488069e2c1e44be5f6
Adam Giergun <adam.giergun@onet.pl>
2022-03-14 12:44:00 +0100

    Code fix

--- app/src/main/java/com/example/ryanair/repository/StationsRepositoryImpl.kt
+++ app/src/main/java/com/example/ryanair/repository/StationsRepositoryImpl.kt
@@ -3,8 +3,6 @@ package com.example.ryanair.repository
 import com.example.ryanair.db.Station
 import com.example.ryanair.network.RyanairApi
 import com.example.ryanair.network.asDbModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class StationsRepositoryImpl @Inject constructor(): StationsRepository {
@@ -18,15 +16,15 @@ class StationsRepositoryImpl @Inject constructor(): StationsRepository {
     override var error = false
         private set
 
-    override suspend fun refresh() = withContext(Dispatchers.IO) {
+    override suspend fun refresh() {
         try {
-            stations = RyanairApi.retrofitStationsApiService.getAirports().asDbModel()
+            stations = RyanairApi.retrofitStationsApiService.getStationsContainer().asDbModel()
             errorText = null
             error = false
         } catch (e: Exception) {
             stations = null
-            errorText = e.localizedMessage
             error = true
+            errorText = e.localizedMessage
         }
     }
 }
